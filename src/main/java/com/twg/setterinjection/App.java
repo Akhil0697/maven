3bec14270681fc6d6b371a2c83503ff53c0363fc
Akhil5131 <113959109+Akhil5131@users.noreply.github.com>
2024-01-07 15:13:27 -0500

    first maven

--- src/main/java/com/twg/setterinjection/App.java
+++ src/main/java/com/twg/setterinjection/App.java
@@ -1,5 +1,8 @@
 package com.twg.setterinjection;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       //create a spring container
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/twg/setterinjection/configmetadata.xml");
+
+        Customer customer  = (Customer) context.getBean("cus");
+        System.out.println("Name:"+customer.getName());
+        System.out.println("Name:"+customer.getContact());
+        System.out.println("Name:"+customer.getAddress());
     }
 }
